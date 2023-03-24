b2f726868c4a499ea870474a16938e1ad24c4aec
Jeong-seo-won <94349879+JSWLDS@users.noreply.github.com>
2023-03-24 09:11:42 +0900

    Update GameList.java

--- pro/project/src/project/GameList.java
+++ pro/project/src/project/GameList.java
@@ -8,12 +8,6 @@ public class GameList {
 	private List<GameInfomation> gameList = new ArrayList<GameInfomation>();
 
 	public GameList() {}
-
-	public GameList(List<GameInfomation> gameList) {
-		super();
-		this.gameList = gameList;
-	}
-	
 	
 	public List<GameInfomation> getGameList() {
 		return gameList;
